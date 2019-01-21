d768f1b285272e7fb09dc2539e5fc7bc743a0b1f
Julian Inwood <jinw@protonmail.com>
2019-01-21 11:03:21 +0000

    Split cookie tests out

--- test/cookies.test.js
+++ test/cookies.test.js
@@ -8,12 +8,25 @@ describe('getCookieValue()', () => {
     it('returns a string value if there is a cookie', () => {
         // Arrange
         const cookie = 'MyCookie=foo';
-        setCookieValue(cookie);
+        document.cookie = cookie;
 
         // Act
         const result = getCookieValue();
 
         // Assert
         expect(result).toBe(`; ${cookie}`);
+    });
+});
+
+describe('setCookieValue()', () => {
+    it('sets the cookie value', () => {
+        // Arrange
+        const cookie = 'MyCookie=foo';
+
+        // Act
+        setCookieValue(cookie);
+
+        // Assert
+        expect(document.cookie).toBe(`; ${cookie}`);
     });
 });
