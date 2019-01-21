5cea21f6efdad6c8ad6a08a9213383f3b128196f
Julian Inwood <jinw@protonmail.com>
2019-01-21 11:06:25 +0000

    Remove space

--- src/cookies.js
+++ src/cookies.js
@@ -9,12 +9,10 @@
  *
  * @returns {string}
  */
-
 export const getCookieValue = () => document.cookie;
 
 /**
  * Set the cookie content.
  *
  */
-
 export const setCookieValue = value => (document.cookie = value);
